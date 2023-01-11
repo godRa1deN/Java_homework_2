43066be5d9eab32900d640f8405df07254be9085
godRa1deN <andrey_2468@mail.ru>
2023-01-11 13:22:03 +0300

    All methods without classes and interfaces

--- src/Main.java
+++ src/Main.java
@@ -7,71 +7,93 @@ import java.nio.file.Paths;
 import java.util.List;
 
 class Main {
-    public static boolean fileExsist(String path, String requare, ArrayList<String> files) {
-        if (files.contains(requare)) {
-            return true;
-        } else {
-            throw new RuntimeException("Wrong requare in " + path);
+    public static void fileExist(String path, String require, ArrayList<String> files) {
+        if (!files.contains(require)) {
+            throw new RuntimeException("Wrong require in " + path);
         }
     }
     public static String readFile(String path, Charset encoding) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get(path), encoding);
         return String.join(System.lineSeparator(), lines);
     }
-    public static String findPath (String fileContent, String filePath) {
-        if (fileContent.contains("requare")) {
-            System.out.println("requare exsist" + " in " + filePath);
-            int requareIndex = fileContent.indexOf("requare");
-            requareIndex += 8;
-            char requareEnd = '\'';
-            String newPath = "";
-            while (fileContent.charAt(requareIndex) != requareEnd) {
-                newPath += fileContent.charAt(requareIndex);
-                requareIndex++;
+    public static ArrayList<String> findPath(String fileContent) {
+        ArrayList<String> requireList = new ArrayList<>();                              //массив всех requare в файле
+        while (fileContent.contains("require")) {
+            int requireIndex = fileContent.indexOf("require");
+            int temp = requireIndex;
+            requireIndex += 8;
+            char requireEnd = '\'';
+            StringBuilder newPath = new StringBuilder();
+            while (fileContent.charAt(requireIndex) != requireEnd) {
+                newPath.append(fileContent.charAt(requireIndex));                            //newPath = содержимое requare
+                requireIndex++;
             }
-            System.out.println(newPath);
-            return newPath;
-        } else {
-            throw new RuntimeException("requare is not found" + " in " + filePath);
+            String firstPart = fileContent.substring(0, temp);
+            String secondPart = fileContent.substring(requireIndex + 1);
+            fileContent = firstPart + secondPart;                                       //удаление requare
+            requireList.add(newPath.toString());
         }
+        return requireList;
     }
 
-    public static ArrayList<File> AllFiles(ArrayList<File> files) {
-
+    public static void AllFiles(ArrayList<File> files) {
         ListIterator<File> iter = files.listIterator();                                     //устанавливаем итератор на начало списка
-
         while(iter.hasNext()) {                                                             //идем до конца списка, пока будут файлы
             File file = iter.next();                                                        //сохраняем в переменную рассматриваемый файл или директорию
             if(file.isDirectory()) {                                                        //если файл - директория
                 int count = iter.nextIndex();                                               //запоминаем положение в списке
                 iter.remove();                                                              //даляем папку
-                files.addAll(Arrays.asList(file.listFiles()));                              //добавляем в конец списка все содержимое удаленной папки
+                files.addAll(Arrays.asList(Objects.requireNonNull(file.listFiles())));                              //добавляем в конец списка все содержимое удаленной папки
                 iter = files.listIterator(count);                                           //устанавливаем итератор на след элемент сразу после удаленной директории
             }
         }
-
-        return files;
+    }
+    public static void getFinalFile(ArrayList<String> sortedList) throws IOException {
+        StringBuilder finalFile = new StringBuilder();
+        for (String s : sortedList) {
+            String content = readFile(s, StandardCharsets.UTF_8);
+            finalFile.append(content);
+        }
+        FileOutputStream fos = new FileOutputStream(".\\src\\root\\answer.txt");
+        fos.write(finalFile.toString().getBytes());
+        fos.flush();
+        fos.close();
     }
 
-    public static void main(String[] args) {
+    public static void cyclesSearch(String path, ArrayList<String> way, ArrayList<String> files) throws IOException {
+        String content = readFile(path, StandardCharsets.UTF_8);
+        ArrayList<String> require = findPath(content);
+        for (String s : require) {
+            fileExist(path, s, files);
+            if (way.contains(s)) {
+                throw new RuntimeException("Cycle found in " + path + " " + s);
+            }
+            way.add(s);
+            cyclesSearch(s, way, files);
+            way.remove(s);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         String pathToDir = "./src/root";                                                  //наша директория
         File dir = new File(pathToDir);
 
-        ArrayList<File> files = new ArrayList<File>(Arrays.asList(dir.listFiles()));        //получаем все файлы из текущей директории
+        ArrayList<File> files = new ArrayList<>(Arrays.asList(Objects.requireNonNull(dir.listFiles())));        //получаем все файлы из текущей директории
 
-        files = AllFiles(files);                                                            //здесь возвращаем список всех файлов из директории и поддерикторий
+        AllFiles(files);                                                                //здесь возвращаем список всех файлов из директории и поддерикторий
 
         Iterator<File> iter = files.iterator();
-        String content = "";
-        String requare = "";
-        List<String> list = new ArrayList<String>();
-        ArrayList<String> allPathes = new ArrayList<String>();
-        while(iter.hasNext()) {                                                            //выводим абсолютные пути к файлам
+        String content;
+        ArrayList<String> sortedList = new ArrayList<>();
+        ArrayList<String> allPaths = new ArrayList<>();
+        ArrayList<String> require;
+        ArrayList<String> way = new ArrayList<>();
+        while (iter.hasNext()) {                                                            //выводим абсолютные пути к файлам
             String path = iter.next().getPath();
-            allPathes.add(path);
+            allPaths.add(path);
         }
         iter = files.iterator();
-        System.out.println(allPathes);
+        System.out.println(allPaths);
         while(iter.hasNext()) {                                                            //выводим абсолютные пути к файлам
             String path = iter.next().getPath();
             try {
@@ -79,11 +101,39 @@ class Main {
             } catch (IOException e) {
                 throw new RuntimeException("File is not found!");
             }
-            allPathes.add(path);
-            requare = findPath(content, path);
-            fileExsist(path, requare, allPathes);
-            //list.add(0, requare);
+            allPaths.add(path);
+            require = findPath(content);
+            System.out.println(require);
+            for (String req : require) {
+                fileExist(path, req, allPaths);
+                if (!req.equals("none")) {
+                    fileExist(path, req, allPaths);
+                    if (sortedList.contains(req) && sortedList.contains(path)) {           //если req < path то swap
+                        if (sortedList.indexOf(req) > sortedList.indexOf(path)) {
+                            Collections.swap(sortedList, sortedList.indexOf(req), sortedList.indexOf(path));
+                        }
+                    }
+                    if (!sortedList.contains(req)) {
+                        sortedList.add(0, req);
+                    }
+                    if (!sortedList.contains(path)) {
+                        sortedList.add(sortedList.size(), path);
+                    }
+                }
+            }
+            if (require.isEmpty()) {
+                if (!sortedList.contains(path)) {
+                    sortedList.add(0, path);
+                } else {
+                    sortedList.remove(path);
+                    sortedList.add(0, path);
+                }
+            }
+            way.clear();
+            way.add(path);
+            cyclesSearch(path, way, allPaths);
         }
-        //System.out.println(list);
+        System.out.println(sortedList);
+        getFinalFile(sortedList);
     }
 }
